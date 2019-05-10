2611621fb6fdeac7f8d210996571dff8ecf9b6b9
Johannes Dörr <mail@johannesdoerr.de>
2019-05-10 13:36:29 -0500

    Fixed dependencies

--- Example/Pods/Target Support Files/Pods-DataSources_Example/Pods-DataSources_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DataSources_Example/Pods-DataSources_Example-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DataSources/DataSources.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Delegates/Delegates.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DataSources/DataSources.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Delegates/Delegates.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
