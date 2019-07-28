04ebb60d88b99b2819ef4c3533a91ea5af69498d
Johannes Dörr <mail@johannesdoerr.de>
2019-07-28 09:35:44 +0200

    Fixed collectionView property beeing optional instead of force-unwrap like in UICollectionViewController

--- DataSources/Classes/ListDataSourceDelegateCollectionViewUpdater.swift
+++ DataSources/Classes/ListDataSourceDelegateCollectionViewUpdater.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public class ListDataSourceDelegateCollectionViewUpdater {
-    public var collectionView: UICollectionView?
+    public var collectionView: UICollectionView!
     
     public init(collectionView: UICollectionView?) {
         self.collectionView = collectionView
--- DataSources/Classes/ListDataSourceDelegateCollectionViewUpdating.swift
+++ DataSources/Classes/ListDataSourceDelegateCollectionViewUpdating.swift
@@ -20,39 +20,39 @@ public protocol ListDataSourceDelegateCollectionViewUpdating: ListDataSourceDele
     /**
      The collectionView that should be updated
      */
-    var collectionView: CollectionView? { get }
+    var collectionView: CollectionView! { get }
 }
 
 extension ListDataSourceDelegateCollectionViewUpdating {
     public func insert(at indexPaths: [IndexPath]) {
-        collectionView?.insertItems(at: indexPaths)
+        collectionView.insertItems(at: indexPaths)
     }
     
     public func delete(at indexPaths: [IndexPath]) {
-        collectionView?.deleteItems(at: indexPaths)
+        collectionView.deleteItems(at: indexPaths)
     }
     
     public func reload(at indexPaths: [IndexPath]) {
-        collectionView?.reloadItems(at: indexPaths)
+        collectionView.reloadItems(at: indexPaths)
     }
     
     public func move(from indexPath: IndexPath, to newIndexPath: IndexPath) {
-        collectionView?.moveItem(at: indexPath, to: indexPath)
+        collectionView.moveItem(at: indexPath, to: indexPath)
     }
     
     public func performBatchUpdates(_ updates: (() -> Void), completion: ((Bool) -> Void)?) {
-        collectionView?.performBatchUpdates(updates, completion: completion)
+        collectionView.performBatchUpdates(updates, completion: completion)
     }
     
     public func reloadData() {
-        collectionView?.reloadData()
+        collectionView.reloadData()
     }
     
     public func select(at indexPath: IndexPath?) {
-        collectionView?.selectItem(at: indexPath, animated: false, scrollPosition: [])
+        collectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
     }
     
     public var selectedIndexPaths: [IndexPath]? {
-        return collectionView?.indexPathsForSelectedItems
+        return collectionView.indexPathsForSelectedItems
     }
 }
